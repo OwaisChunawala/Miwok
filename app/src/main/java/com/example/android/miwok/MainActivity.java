5e37167ec1219fc799eb5003b8a752af3de82943
Owais Chunawala <chunawalaowais@gmail.com>
2017-06-06 22:43:51 +0530

    added activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openColorsActivity(View view){
+        Intent intentColors = new Intent(this, ColorsActivity.class);
+        if (intentColors.resolveActivity(getPackageManager()) != null) {
+                startActivity(intentColors);
+        }
+    }
+    public void openFamilyActivity(View view){
+        Intent intentFamily = new Intent(this, FamilyActivity.class);
+        if (intentFamily.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentFamily);
+        }
+    }
+    public void openNumbersActivity(View view){
+        Intent intentNumbers = new Intent(this, NumbersActivity.class);
+        if (intentNumbers.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentNumbers);
+        }
+    }
+    public void openPhrasesActivity(View view){
+        Intent intentPhrases = new Intent(this, PhrasesActivity.class);
+        if (intentPhrases.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentPhrases);
+        }
     }
 }
